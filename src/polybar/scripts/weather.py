c2b06ddf9a57552ed59d5ebcdab5aeb8b86ac224
neerajadhav <adhavneeraj9500@gmail.com>
2023-10-17 13:57:04 +0530

    Changes to be committed: 	modified:   src/polybar/config.ini 	modified:   src/polybar/scripts/weather.py

--- src/polybar/scripts/weather.py
+++ src/polybar/scripts/weather.py
@@ -15,8 +15,8 @@
 
 import requests
 
-CITY = "2803138"
-API_KEY = "756edce7e9d4c385ef9499a53492678c"
+CITY = "1256259"
+API_KEY = "02bb8ff71fe9260f8046e1f942bb5ab8"
 UNITS = "Metric"
 UNIT_KEY = "C"
 #UNIT_KEY = "F"
