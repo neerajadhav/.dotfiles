49dabb3f8ceac070d3ef06d9ef6436398b9b3f2e
neerajadhav <neerajadhav@duck.com>
2024-03-31 09:59:57 +0530

    modified:   src/i3/autostart.sh 	modified:   src/i3/config 	deleted:    src/i3/lock.sh

--- src/i3/autostart.sh
+++ src/i3/autostart.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-# Set wal colors
+# Set wal colors and lockscreen
 grep -Po "(?<=file=).+" ~/.config/nitrogen/bg-saved.cfg | while read -r wallpaper; do
     # sh ~/.config/polybar/shades/scripts/pywal.sh  "$wallpaper"
     wal -i "$wallpaper"
+    betterlockscreen -u "$wallpaper"
 done
 
 autotiling -l 2 &
